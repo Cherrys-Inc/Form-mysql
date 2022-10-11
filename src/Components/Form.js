b4dfbebc2b4a53915e1de903936a7b5da02b1c27
anishaf92 <68312300+anishaf92@users.noreply.github.com>
2022-10-11 09:57:37 +0530

    Changed individual variable to an object

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,59 +5,22 @@ import { useNavigate } from 'react-router-dom';
 import { FilePicker } from 'evergreen-ui';
 import "./form.css"
 const Form = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [mobile, setMobile] = useState("");
-    const [dob, setDob] = useState(new Date());
-    const [course,setCourse] = useState("Maths");
-    const [feedback,setFeedback] = useState("");
-    const [gender,setGender] = useState("");
-    const [agree,setAgree] = useState(false);
-    const [file,setFile] = useState("");
+      const [userData,setUserData] = useState({
+        firstname: "",
+        lastname: "",
+        email:"",
+        mobile:"",
+        dob:new Date(),
+        course:"",
+        feedback:"",
+        gender:"",
+        agree:false
+    })
+    const [file,setFile]=useState("");
+   
     const navigate = useNavigate();
     
-    const handleChangecourse =(e)=>{
-        setCourse(e.target.value)
 
-    }
-    const handleChangetext =(e)=>{
-        setFeedback(e.target.value)
-
-    }
-    const handleFirstName=(e)=>{
-        
-        
-        setFirstName(e.target.value)
-        
-
-    }
-    const handleLastName=(e)=>{
-        
-        setLastName(e.target.value)
-        
-
-    }
-    const handleEmail=(e)=>{
-        
-       
-        setEmail(e.target.value)
-    
-
-    }
-    const handleMobile=(e)=>{
-        
-        
-        setMobile(e.target.value)
-        
-
-    }
-    const handleAgree=(e)=>{
-        
-        
-            setAgree(!agree)
-        
-    }
     const validateForm =(e)=>{
         e.preventDefault();
        if(firstName === "")
@@ -112,33 +75,33 @@ const Form = () => {
               <div className="username">
                   <label className="form__label" >First Name <span className="mandatory">*</span> </label>
 
-                  <input className="form__input" type="text" onChange={e => handleFirstName(e) } placeholder="First Name"/>
+                  <input className="form__input" type="text" onChange={e => setUserData({...userData, firstname: e.target.value}) } placeholder="First Name"/>
               </div>
               <div className="lastname">
                   <label className="form__label" >Last Name </label>
-                  <input  type="text" name="" onClick={e => handleLastName(e)}  className="form__input"placeholder="LastName"/>
+                  <input  type="text" name="" onClick={e => setUserData({...userData, lastname: e.target.value})}  className="form__input"placeholder="LastName"/>
               </div>
               <div className="email">
-                  <label className="form__label" >Email <span className="mandatory"> * </span> </label>
-                  <input  type="email" onChange={e => handleEmail(e)} className="form__input" placeholder="Email"/>
+                  <label className="form__label" >Email <span className="mandatory"> *</span> </label>
+                  <input  type="email" onChange={e => setUserData({...userData, email: e.target.value})} className="form__input" placeholder="Email"/>
               </div>
               <div className="mobile">
                   <label className="form__label" >Mobile Number <span className="mandatory"> * </span> </label>
-                  <input className="form__input" type="text"  onChange={e => handleMobile(e)} placeholder="Mobile NUmber"/>
+                  <input className="form__input" type="text"  onChange={e => setUserData({...userData, mobile: e.target.value})} placeholder="Mobile NUmber"/>
               </div>
               <div className="date">
               <label className="form__label" >Date of Birth <span className="mandatory"> * </span></label>
-                <DatePicker dateFormat="MM/dd/yyyy"   onChange={setDob} value={dob} />
+                <DatePicker dateFormat="MM/dd/yyyy"   onChange={setUserData({...userData, dob: setDob})} value={dob} />
               </div>
               <div className='gender'  >
               Select your gender: <span className="mandatory"> * </span>
              <div className="gen-sel">
-                <input type="radio" id="gen-male" name="gen-ans" onChange={e=>setGender(e.target.value)} value="Male" />Male
-                <input type="radio" id="gen-female" name="gen-ans" onChange={e=>setGender(e.target.value)} value="Female" />Female
+                <input type="radio" id="gen-male" name="gen-ans" onChange={e => setUserData({...userData, gender: e.target.value})} value="Male" />Male
+                <input type="radio" id="gen-female" name="gen-ans" onChange={e=> setUserData({...userData, gender: e.target.value})} value="Female" />Female
             </div>
             <label>
                 Select course: <span className="mandatory"> * </span>
-                <select value={course} onChange={e=>setCourse(e.target.value)}>
+                <select value={course} onChange={e=>setUserData({...userData, course: e.target.value})}>
                 <option value="Maths">Maths</option>
                 <option value="Physics">Physics</option>
                 <option value="Chemistry">Chemistry</option>
@@ -147,14 +110,14 @@ const Form = () => {
             </label>
             <div>
             Enter your suggestions:
-            <textarea value={feedback} onChange={e => setFeedback(e.target.value)} />
+            <textarea value={feedback} onChange={e => setUserData({...userData, feedback: e.target.value})} />
             </div>
             <div>
                 <label>Upload your photo</label>
                 <FilePicker
                 width={200}
                 placeholder="Select your file"
-                onChange={setFile}
+                onChange={e => setFile(e.target.value)}
                 />
             </div>
 
@@ -163,7 +126,7 @@ const Form = () => {
               <span className="mandatory">*</span>
              <div className="ack">
                 <input type="checkbox" id="terms" checked={agree}
-          onChange={e=>handleAgree(e) } name="terms-ans" value="Agree" />Agree
+          onChange={e => setUserData({...userData, email: e.target.value}) } name="terms-ans" value="Agree" />Agree
             </div>
             </div>
           </div>
