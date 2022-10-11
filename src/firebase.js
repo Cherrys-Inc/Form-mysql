c1e63b524d8f7e07d12a5a25f0f94c367df83b14
anishaf92 <68312300+anishaf92@users.noreply.github.com>
2022-10-11 19:51:15 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,9 @@ import { initializeApp } from "firebase/app";
 import { FacebookAuthProvider, getAuth,GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import {firebaseConfig} from "./firebaseConfig.env"
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDGRTc2mZAbIWEpK9bH07U7V1HTDtGD71o",
-  authDomain: "login-authentication-415dd.firebaseapp.com",
-  projectId: "login-authentication-415dd",
-  storageBucket: "login-authentication-415dd.appspot.com",
-  messagingSenderId: "575569779145",
-  appId: "1:575569779145:web:4bc25de790c6893c837018"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
