8983126a2de7eae40abff63d5a910eb2bfb145a8
anishaf92 <68312300+anishaf92@users.noreply.github.com>
2022-10-13 18:22:38 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,16 @@ import { initializeApp } from "firebase/app";
 import { FacebookAuthProvider, getAuth,GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import {firebaseConfig} from "./firebaseConfig.env"
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
 // Your web app's Firebase configuration
 
 
