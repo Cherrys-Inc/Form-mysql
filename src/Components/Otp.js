b8089992e66af5f1195213f631e7c4c7c63208fa
anishaf92 <68312300+anishaf92@users.noreply.github.com>
2022-10-14 08:34:31 +0530

    Update Otp.js

--- src/Components/Otp.js
+++ src/Components/Otp.js
@@ -6,35 +6,35 @@ const Otp = ({ verifyNo, otpSubmit, viewOtpForm}) => {
  
     return (<div>
          {!viewOtpForm ? (
-        <div className="form-wrapper">
-          <form id="loginForm" onSubmit={verifyNo}>
-            <div className="input-field">
+        <div className = "form-wrapper">
+          <form id = "loginForm" onSubmit = {verifyNo}>
+            <div className = "input-field">
               <label>Phone Number</label>
               <input
-                type="text"
-                placeholder="Phone"
-                name="phone"
-                autoComplete="false"
+                type = "text"
+                placeholder = "Phone"
+                name = "phone"
+                autoComplete = "false"
               />
             </div>
-            <button className="main-button" type="submit" id="sign-in-button">
+            <button className = "main-button" type = "submit" id = "sign-in-button">
               Verify Mobile Number
             </button>
           </form>
         </div>
       ) : (
-        <div className="form-wrapper" onSubmit={otpSubmit}>
-          <form id="otpForm">
-            <div className="input-field">
+        <div className = "form-wrapper" onSubmit = {otpSubmit}>
+          <form id = "otpForm">
+            <div className = "input-field">
               <label>Enter OTP</label>
               <input
-                type="number"
-                placeholder="One time password"
-                name="otp_value"
-                autoComplete="false"
+                type = "number"
+                placeholder = "One time password"
+                name = "otp_value"
+                autoComplete = "false"
               />
             </div>
-            <button className="main-button" type="submit">
+            <button className = "main-button" type = "submit">
               Verify OTP
             </button>
           </form>
